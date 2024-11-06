678923dd119f644207fa0918048d0c7276e280b3
Pavel <68122101+red-prig@users.noreply.github.com>
2024-11-06 21:42:35 +0300

    +

--- sys/sys_bootparam.pas
+++ sys/sys_bootparam.pas
@@ -12,7 +12,7 @@ const
  CPUID_NEO_MODE =$740f00;
 
 var
- p_system_sdk_version :DWORD=$10010001; // $10010001;
+ p_system_sdk_version :DWORD=$11008001; // $10010001;
  p_cpuid              :DWORD=CPUID_BASE_MODE; //base mode = 0x710f13 / neo mode = 0x740f00
  p_base_ps4_mode      :DWORD=1; //[0..1]
  p_neomode            :DWORD=0; //[0..1]
