ec161c820561323c27d35d6b94aaf697cd8cbff8
Pavel <68122101+red-prig@users.noreply.github.com>
2024-11-08 21:15:04 +0300

    +

--- src/np/ps4_libscenpgameintent.pas
+++ src/np/ps4_libscenpgameintent.pas
@@ -1,11 +1,12 @@
 unit ps4_libSceNpGameIntent;
 
 {$mode objfpc}{$H+}
+{$CALLING SysV_ABI_CDecl}
 
 interface
 
 uses
-  ps4_program;
+ subr_dynlib;
 
 implementation
 
@@ -16,23 +17,23 @@ const
 type
  pSceNpGameIntentInitParam=^SceNpGameIntentInitParam;
  SceNpGameIntentInitParam=packed record
-  size:QWORD;
+  size    :QWORD;
   reserved:array[0..31] of Byte;
  end;
 
  pSceNpGameIntentData=^SceNpGameIntentData;
  SceNpGameIntentData=packed record
-  data:array[0..SCE_NP_GAME_INTENT_DATA_MAX_SIZE-1] of Byte;
+  data   :array[0..SCE_NP_GAME_INTENT_DATA_MAX_SIZE-1] of Byte;
   padding:array[0..6] of Byte;
  end;
 
  pSceNpGameIntentInfo=^SceNpGameIntentInfo;
  SceNpGameIntentInfo=packed record
-  size:QWORD;
-  userId:Integer;
+  size      :QWORD;
+  userId    :Integer;
   intentType:array[0..SCE_NP_GAME_INTENT_TYPE_MAX_SIZE-1] of AnsiChar;
-  padding:array[0..6] of Byte;
-  reserved:array[0..255] of Byte;
+  padding   :array[0..6] of Byte;
+  reserved  :array[0..255] of Byte;
   intentData:SceNpGameIntentData;
  end;
 
@@ -59,20 +60,23 @@ begin
 end;
 
 
-function Load_libSceNpGameIntent(Const name:RawByteString):TElf_node;
+function Load_libSceNpGameIntent(name:pchar):p_lib_info;
 var
- lib:PLIBRARY;
+ lib:TLIBRARY;
 begin
- Result:=TElf_node.Create;
- Result.pFileName:=name;
- lib:=Result._add_lib('libSceNpGameIntent');
- lib^.set_proc($9BCEC11F1B7F1FAD,@ps4_sceNpGameIntentInitialize);
- lib^.set_proc($8C4217500AFD5C4F,@ps4_sceNpGameIntentReceiveIntent);
- lib^.set_proc($ACF97420D35CFCCF,@ps4_sceNpGameIntentGetPropertyValueString);
+ Result:=obj_new_int('libSceNpGameIntent');
+
+ lib:=Result^.add_lib('libSceNpGameIntent');
+ lib.set_proc($9BCEC11F1B7F1FAD,@ps4_sceNpGameIntentInitialize);
+ lib.set_proc($8C4217500AFD5C4F,@ps4_sceNpGameIntentReceiveIntent);
+ lib.set_proc($ACF97420D35CFCCF,@ps4_sceNpGameIntentGetPropertyValueString);
 end;
 
+var
+ stub:t_int_file;
+
 initialization
- ps4_app.RegistredPreLoad('libSceNpGameIntent.prx',@Load_libSceNpGameIntent);
+ RegisteredInternalFile(stub,'libSceNpGameIntent.prx',@Load_libSceNpGameIntent);
 
 end.
 
